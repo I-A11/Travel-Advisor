e20b204c7590396a090b87c2b472251a6ff16aaf
I-A11 <d.islam1983@gmail.com>
2021-08-22 15:18:05 +1000

    create component folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from "./component/Header/Header";
+import List from "./component/List/List";
+import Map from "./component/Map/Map";
+import { grid } from "@material-ui/system";
+
 function App() {
   return (
-    <div>
-      <h1>HIIIIIII</h1>
-      <p>Go React Go Go Go </p>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
